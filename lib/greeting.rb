8242cfd13820c0f25c88b77c66982ff2d5df5224
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-15 04:54:17 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,5 @@
 # code the #greeting method here!
 
 def greeting(name)
-  puts "Hi, I am HAL. What's your name?"
-  name = gets.strip
-  puts "Hi #{name}. Nice to see you again."
+  puts "Hello #{name}. It's nice to meet you."
 end
